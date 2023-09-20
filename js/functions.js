1859b85d172d1892c54037def97b835e1e798221
Daniel Bastos <bastosdaniel50@gmail.com>
2023-09-20 08:43:50 -0300

    add scrolltop animate in js

--- js/functions.js
+++ js/functions.js
@@ -4,4 +4,13 @@ $(function(){
         $('.mobile-menu').find('ul').slideToggle();
     }); 
 
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offsetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop':offsetTop});
+
+        return false;
+    });
+
 });
